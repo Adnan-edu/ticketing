0971c532e677ae0c42708b7648f9c9a019141a3b
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-08 23:34:42 +1100

    added not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,15 +5,21 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
 
 app.use(currentUserRouter);
 app.use(signupRouter);
+app.all('*',async (req, res)=>{
+  throw new NotFoundError();
+})
 app.use(errorHandler);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+
 
 
 app.listen(3000, () => {
