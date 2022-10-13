3ee72bf68321a1ef557193fafa8d0fb33f951133
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-13 22:27:53 +1100

    common request validation middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,6 @@
 import express, {Request, Response} from 'express';
-import {body, validationResult} from 'express-validator';
-import { RequestValidationError } from '../errors/request-validation-error';
+import {body} from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -14,11 +14,9 @@ router.post('/api/users/signin', [
         .withMessage('You must supply a password.')
 
 
-],(req: Request, res: Response)=>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        throw new RequestValidationError(errors.array());
-    }
+],
+validateRequest,
+(req: Request, res: Response)=>{
     res.send("Hi there!!!");
 });
 
