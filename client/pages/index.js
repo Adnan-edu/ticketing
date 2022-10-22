5fc37a33d7e229fb730888e3a73b37e2e62f7fe7
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-22 23:59:24 +1100

    getInitialProps, ingress-nginx namespace and service conf and passing through cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,27 @@
-const testFunc = () => {
-    return <div>Hello everyone!!!!</div>
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    //console.log(currentUser);
+    console.log(currentUser);
+    return <div>Landing Page</div>
 }
-export default testFunc;
+LandingPage.getInitialProps = async (req) =>{
+    if(typeof window === 'undefined'){
+        //Then we are in server
+        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
+            headers: req.headers
+        });
+        return data;
+    }else{
+        //Then we are in browser
+        const {data} = await axios.get('/api/users/currentuser');
+        return data;
+    }
+    return {};
+    //const response = await axios.get('/api/users/currentuser');
+    // .catch((err) => {
+    //     console.log(err.message);
+    //   });
+    //return response.data;
+}
+export default LandingPage;
