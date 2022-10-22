02b28e8eeda0016dbb07704383853db9fe3fc3da
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-22 16:23:08 +1100

    Added useRequest hooks to avoid duplicate api call

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,21 @@
 import { useState } from 'react';
 import axios from 'axios';
+import useRequest from '../../hooks/use-request';
 
 const signUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const {errors, doRequest} = useRequest({
+      url: '/api/users/signUp',
+      method: 'post',
+      body: {
+        email, password
+      }
+    });
   
     const onSubmit = async (event) => {
       event.preventDefault();
-  
-      try{
-        const response = await axios.post('/api/users/signup', {
-          email, password
-        });
-        console.log(response.data);
-      }catch(err){
-        setErrors(err.response.data.errors);
-      }
+      doRequest();
     };
 
     return (
@@ -39,16 +38,7 @@ const signUp = () => {
               className="form-control"
             />
           </div>
-          {errors.length > 0 && (
-            <div className="alert alert-danger">
-              <h4>Ooops....</h4>
-              <ul className="my-0">
-                {errors.map(err => (
-                  <li key={err.message}>{err.message}</li>
-                ))}
-              </ul>
-            </div>
-          )}
+          {errors}
           <button className="btn btn-primary">Sign Up</button>
         </form>
       );
