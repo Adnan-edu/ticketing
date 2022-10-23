6324d3a3f0405d578772c85ae3db08f7abcc5cc3
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-23 17:27:16 +1100

    added reusable api client - buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,23 +1,15 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({currentUser}) => {
     //console.log(currentUser);
     console.log(currentUser);
     return <div>Landing Page</div>
 }
-LandingPage.getInitialProps = async (req) =>{
-    if(typeof window === 'undefined'){
-        //Then we are in server
-        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',{
-            headers: req.headers
-        });
-        return data;
-    }else{
-        //Then we are in browser
-        const {data} = await axios.get('/api/users/currentuser');
-        return data;
-    }
-    return {};
+LandingPage.getInitialProps = async (context) =>{
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
+    
     //const response = await axios.get('/api/users/currentuser');
     // .catch((err) => {
     //     console.log(err.message);
