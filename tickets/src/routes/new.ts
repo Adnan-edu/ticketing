d23b52093777455795020355040aadbfe39dae78
mdashikadnan <md.ashikadnan@gmail.com>
2022-10-29 22:38:04 +1100

    Testing request validation and validating title and price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,9 +1,17 @@
-import express from 'express';
-import { requireAuth } from '@adnan-edu-tickets/common';
-
+import express, {Request, Response} from 'express';
+import { requireAuth, validateRequest } from '@adnan-edu-tickets/common';
+import { body } from 'express-validator';
 const routers = express.Router();
 
-routers.post('/api/tickets', requireAuth, (req, res) => {
+routers.post('/api/tickets', requireAuth, 
+[
+  body('title').not().isEmpty().withMessage('Title is required'),
+  body('price')
+    .isFloat({ gt: 0 })
+    .withMessage('Price must be greater than 0'),
+],
+validateRequest,
+(req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
