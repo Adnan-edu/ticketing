33001d2029ad4f7c8faa1a246acd7c21a4a88459
mdashikadnan <md.ashikadnan@gmail.com>
2022-11-05 15:00:18 +1100

    publishing ticket update event

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from '@adnan-edu-tickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -32,6 +34,15 @@ router.put(
       price: req.body.price
     });
     await ticket.save();
+    //publish Ticket update event
+
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
+
     res.send(ticket);
   }
 );
