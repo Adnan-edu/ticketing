e6fbdade752c954617d3cb7d63b1aaf92b30ff6a
mdashikadnan <md.ashikadnan@gmail.com>
2022-11-07 16:57:54 +1100

    added order service initially and scaffolding

---
+++ orders/src/app.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import 'express-async-errors';
+import { json } from "body-parser";
+
+import { errorHandler, NotFoundError, currentUser } from '@adnan-edu-tickets/common';
+import cookieSession from "cookie-session";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import {updateTicketRouter} from "./routes/update"
+
+
+
+const app = express();
+//express trust traffic as being secured even though it's coming from the proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,    //JWT already encrypted, disable encryption
+  secure: process.env.NODE_ENV !== 'test'      //Only be used over HTTPS connections
+}));
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async(req, res)=>{
+  throw new NotFoundError();
+})
+app.use(errorHandler);
+
+export { app };
