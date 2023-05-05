f49ecc119c28a9a09cc8208b6f0bcfe8e4f35f2a
mdashikadnan <md.ashikadnan@gmail.com>
2023-05-06 03:06:18 +1000

    Payment service initial setup with model and deployment file

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import 'express-async-errors';
+import { json } from "body-parser";
+
+import { errorHandler, NotFoundError, currentUser } from '@adnan-edu-tickets/common';
+import cookieSession from "cookie-session";
+
+
+
+
+const app = express();
+//express trust traffic as being secured even though it's coming from the proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,    //JWT already encrypted, disable encryption
+  secure: process.env.NODE_ENV !== 'test'      //Only be used over HTTPS connections
+}));
+app.use(currentUser);
+
+app.all('*', async(req, res)=>{
+  throw new NotFoundError();
+})
+app.use(errorHandler);
+
+export { app };
